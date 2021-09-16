a32b9e0616b8e25e37230a237cf1dbf2b206df7e
fredcallaway <fredc@llaway.com>
2021-09-16 07:38:41 -0400

    StartRepl and JumpCell

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sublime_plugin import WindowCommand
+from sublime import Region
+
 import sublime
 import subprocess
-
+import os
 ITERM = os.path.join(os.path.dirname(__file__), "iterm.applescript")
 
 def osascript(*args):
@@ -26,7 +28,10 @@ class StartTerm(WindowCommand):
     def run(self, ssh=None, app='terminus', **kwargs):
         session = self.window.extract_variables().get("project_base_name", 'default')
         CC = '-CC' if app == 'iterm' else ''
-        cmd = 'tmux {} new-session -A -s {}'.format(CC, session)
+        tmux = 'tmux'
+        if ssh in ('g1', 'g2', 'scotty'):
+            tmux = '~/bin/tmux'
+        cmd = '{} {} new-session -A -s {}'.format(tmux, CC, session)
         if ssh:
             cmd = "ssh -t {} '{}'".format(ssh, cmd)
 
@@ -36,11 +41,80 @@ class StartTerm(WindowCommand):
             sublime.set_timeout_async(self.move_to_right)
         elif app == 'iterm':
             osascript(ITERM, cmd, "True")
-            
-
 
     def move_to_right(self):
         view = self.window.active_view()
         if self.window.num_groups() == 1:
             self.window.run_command("create_pane", dict(direction="right"))
         self.window.set_view_index(view, 1, len(self.window.views_in_group(1)))
+
+
+class StartRepl(WindowCommand):
+    def run(self, restart=False):
+        vars = self.window.extract_variables()
+        file, extension = vars['file_name'], vars['file_extension']
+        cmd = {
+            'jl': 'julia',
+            'r': 'r',
+            'rmd': 'r',
+            'py': 'ipython'
+        }.get(extension.lower(), None)
+
+        def send(s):
+                self.window.run_command("terminus_send_string", {"string": s})
+
+        if restart:
+            if cmd is not None:
+                send("")
+                send(cmd + '\n')
+        else:
+            send("c,")  # ctrl-t c ctrl-t ,
+            send("" + file + "\n")  # cmd-backspace, then file name
+            send("cd " + vars['file_path'] + "\n")
+
+
+            if cmd is not None:
+                send(cmd + '\n')
+
+def find(view, sel, pattern, backward=False):
+    if backward:
+        regions = view.find_all(pattern)
+        start = 0
+        for i in range(len(regions)):
+            if regions[i].end() >= sel.begin():
+                if i == 0:
+                    return Region(0, 0)
+                else:
+                    return regions[i-1]
+        return regions[-1]
+    else:
+        return view.find(pattern, sel.end())
+
+
+class JumpCell(WindowCommand):
+    def run(self, backward=False):
+        view = self.window.active_view()
+        sels = view.sel()
+        
+        pattern = {
+            'R Markdown': r'^```{r.*}\n',
+            'LaTeX': r'^\\\w+section.*\n',
+        }.get(view.syntax().name, r'^# %%.*\n')
+
+        region = find(view, sels[0], pattern, backward)
+        if region.a == -1:
+            return
+        
+        sels.clear()
+        sels.add(region.b)
+        self.window.run_command("show_at_center")
+
+        self.window.run_command("move", {
+            "by": "characters", "forward": True
+        })
+
+        self.window.run_command("move", {
+            "by": "characters", "forward": False
+        })
+
+        
