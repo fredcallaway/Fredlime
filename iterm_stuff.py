a96c682077a7d08252687f359dac32ebcf48c6b9
Fred Callaway <fredcallaway@fb.com>
2022-10-18 20:32:50 -0400

    lazy git correctly finds existing session

--- iterm_stuff.py
+++ iterm_stuff.py
@@ -12,9 +12,40 @@ class LazyGit(WindowCommand):
 
     def test(self):
         try:
-            iterm2.run_until_complete(lazygit)
+            iterm2.run_until_complete(self.lazygit)
         except:
             print('lazygit error')
+
+    async def lazygit(self, connection):   
+        app = await iterm2.async_get_app(connection)
+        
+        # Foreground the app
+        await app.async_activate()
+
+        # check if already exists and activate
+        for window in app.windows:
+            for tab in window.tabs:
+                for session in tab.sessions:
+                    folder = await session.async_get_variable("user.lazygit-folder")
+                    print('folder', folder)
+                    if folder == self.folder:
+                        print('Found session')
+                        await session.async_activate()
+                        return
+
+        # self.folder = '/Users/fredcallaway/projects/recstrats'
+
+        # Start in a new tab or window
+        cmd = f"zsh -dfic 'cd \"{self.folder}\" && /Users/fredcallaway/bin/lazygit'"
+        myterm = app.current_terminal_window
+        if not myterm:
+            myterm = await iterm2.Window.async_create(connection, command=cmd)
+        else:
+            tab = await myterm.async_create_tab(command=cmd)
+
+        session = myterm.current_tab.current_session
+        await session.async_set_variable("user.lazygit-folder", self.folder)
+        await myterm.async_activate()
 
 
 async def test(connection):
@@ -22,26 +53,3 @@ async def test(connection):
     session = app.current_terminal_window.tabs[0].sessions[0]
     await session.async_send_text('x = 1\n')
 
-
-async def lazygit(connection):   
-    app = await iterm2.async_get_app(connection)
-    folder = '/Users/fredcallaway/projects/recstrats'
-    
-    # Foreground the app
-    await app.async_activate()
-
-    # self.folder = '/Users/fredcallaway/projects/recstrats'
-
-    # Start in a new tab or window
-    cmd = f"zsh -dfic 'cd \"{folder}\" && /Users/fredcallaway/bin/lazygit'"
-    myterm = app.current_terminal_window
-    if not myterm:
-        myterm = await iterm2.Window.async_create(connection, command=cmd)
-    else:
-        tab = await myterm.async_create_tab(command=cmd)
-
-    session = myterm.tabs[0].sessions[0]
-    print('about to set variable')
-    await session.async_set_variable("user.testvar", 'test')
-    print('success!')
-    await myterm.async_activate()
