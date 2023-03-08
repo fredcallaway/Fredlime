4f248ec12634fa4be6bf9ae98d3260d398c66193
fredcallaway <fredc@llaway.com>
2023-03-08 13:41:46 -0500

    add insert_file_path

---
+++ insert_file_path.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin
+import subprocess
+
+
+class InsertFilePath(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        # self.window.show_input_panel("Goto Line:", "", self.on_done, None, None)
+        self.window = self.view.window()
+        folder = self.window.extract_variables().get('folder')
+        if folder is None:
+            return
+        fd = 'fd --exclude .git --exclude .cache --no-ignore-vcs'.split(' ')
+        files = subprocess.check_output([*fd, ".", folder]).decode().strip().split('\n')
+        self.files = [f.replace(folder + '/', '') for f in files]
+        self.window.show_quick_panel(self.files, self.on_select)
+        pass
+
+    def on_select(self, i):
+        print(i)
+        self.view.run_command("insert", {"characters": self.files[i]})
+
+    def on_done(self, text):
+        print('Done!', text)
+        try:
+            line = int(text)
+            if self.window.active_view():
+                self.window.active_view().run_command("goto_line", {"line": line} )
+        except ValueError:
+            pass
