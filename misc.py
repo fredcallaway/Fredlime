04ae97b8bb14335a60056ea28d48acac15ba6128
fredcallaway <fredc@llaway.com>
2023-08-14 11:50:57 -0400

    new julia script command

--- misc.py
+++ misc.py
@@ -10,3 +10,15 @@ class PreviewFile(WindowCommand):
     def run(self, **kwargs):
         # print("PreviewFile", kwargs.get("file", 'NO FILE'))
         self.window.open_file(kwargs["file"], sublime.ENCODED_POSITION | sublime.TRANSIENT)
+
+
+class NewJuliaScript(WindowCommand):
+    def run(self):
+        self.window.show_input_panel("Script Name", "", self.create, None, None)
+
+    def create(self, name):
+        self.window.open_file(f"{name}.jl")
+        folder = self.window.extract_variables()['folder']
+        main = os.path.join(folder, "main.jl")
+        with open(main, "a") as f:
+            f.write(f'include("{name}.jl")\n')
